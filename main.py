20ea0c0267745f3a9e0762f88696ddcaf8fb2f01
owoborode oluwaseye <owoborodeseye@gmail.com>
2023-09-07 01:01:19 +0100

    Fix spelling issue

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ async def stage_one_task(slack_name="null", track="null"):
       "track": track,
       "github_file_url": "https://github.com/username/repo/blob/main/staging/main.py",
       "github_repo_url": "https://github.com/blacdev/HNGx-repo",
-      "status": 200
+      "status_code": 200
   }
      
   except Exception as e:
